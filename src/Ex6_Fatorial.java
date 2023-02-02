e00ac0219063c537187cedba2f634c544460a996
mboccall <mboccalletti@gmail.com>
2023-02-02 16:08:34 -0300

    first commit

---
+++ src/Ex6_Fatorial.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ex6_Fatorial {
+    public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Fatorial: ");
+        int fatorial = scan.nextInt();
+
+        int multiplicacao = 1;
+
+        System.out.println(fatorial + "! = ");
+        for(int i = fatorial; i >= 1; i = i - 1){
+            multiplicacao = multiplicacao * i;
+        }
+    
+        System.out.println(multiplicacao);
+
+    }
+}
